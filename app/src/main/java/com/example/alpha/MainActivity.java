8206c2aab4e3f556974ac49841cedab0d81a749f
Hadarmalul <hm6267@bs.amalnet.k12i.il>
2020-11-05 15:50:56 +0200

    .

---
+++ app/src/main/java/com/example/alpha/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.alpha;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void auth(View view) {
+
+        Intent Si = new Intent(this,Authentication.class);
+        startActivity(Si);
+    }
+}
